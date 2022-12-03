e838491f16fcb2ec650f3cc4b030e5f4690b6aa7
mafugo <mahmutfurkangon16400@gmail.com>
2022-12-03 23:16:00 +0300

    delete unnecessary part of labeling simulation

--- hw2/codes/gon_furkan.lua
+++ hw2/codes/gon_furkan.lua
@@ -11,27 +11,10 @@ do
     end
      
      
-    -- no labeled exit
+    -- no labeled exit in Lua
      
     print("-------------------------------")
      
-    -- shows continue simulation in Lua
-    -- continue statement affects only one loop
-    for j= 0, 4, 1 
-    do
-        if j==2 then goto continue end
-        print("innerLoopContinueUnlabeled: ", j)
-        ::continue::
-    end
-     
-    print("-------------------------------")
-     
-    -- shows exit for more than one loop using goto
-    -- goto statement goes to break statement which exits
-    -- from outerLoop and ends the program
-    for j = 0, 4, 1 
-    do
-        if j==3 then goto breakOuterLoop end
-        print("outerLoopBreak: ", j)
-    end
-   ::breakOuterLoop::  break 
+    -- no continue in Lua
+    -- no labeled exit in Lua    
+end
--- hw2/codes/gon_furkan.py
+++ hw2/codes/gon_furkan.py
@@ -1,4 +1,3 @@
-outer_flag = False
 for i in range(5):
     print("------------------------------")
     # shows unlabeled exit
@@ -10,19 +9,9 @@ for i in range(5):
     
     print("------------------------------")
 
-    inner_flag = False # flag to simulate label for an inner loop
 
-    # shows labeled exit using flag
-    # break statement exits only one loop
-    for j in range(5):
-        if j==1:
-            inner_flag = True
-        if inner_flag:
-            break
-        print("innerLoopBreakLabeled: ", j)
-
-    print("------------------------------")
-
+    # no labeled exit in python
+    
     # shows unlabeled continue 
     # continue statement affects only one loop
     for j in range(5):
@@ -32,15 +21,6 @@ for i in range(5):
     
     print("------------------------------")
 
-    # no labeled continue exists in python, 
-    # neither can I simulate it using flags
+    # no labeled continue in python
 
-    # shows simulation of labeled exit for more than one loop
-    # break statement exits from outerLoop and ends the program
-    for j in range(5):
-        if j==3:
-            outer_flag = True
-            break
-        print("outerLoopBreakLabeled: ", j)
-    if outer_flag: 
-        break
+    # no labeled exit in python
